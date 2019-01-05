505e0382729f88e68289891f6bde4623e159fdc9
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 21:16:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,10 +23,11 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card
-  deal_card
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
   display_card_total(card_total)
-  return 
+  return card_total
 end
 
 def hit?(card_total)

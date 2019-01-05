dbdc522efe72402198f40cd89db34e40486f185d
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 21:22:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,10 +33,10 @@ end
 def hit?(card_total)
   prompt_user
   user_input = get_user_input
-  if user_input = "h"
+  if user_input == "h"
     card_total = card_total + deal_card
     return card_total
-  elsif user_input = "s"
+  elsif user_input == "s"
     return card_total
   else
     return invalid_command

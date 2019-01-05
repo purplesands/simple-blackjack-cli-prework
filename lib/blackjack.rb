da2e0abe341204ba6aa78507e9182251b0db561c
Joe Mullen <mullenjoe13@gmail.com>
2019-01-05 21:28:08 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,6 +56,7 @@ def runner
   card_total = initial_round
   until card_total > 21
     card_total = hit?(card_total)
+  end
   end_game(card_total)
   display_card_total(card_total)
   return card_total
